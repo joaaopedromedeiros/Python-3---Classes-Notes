2717b9ab0a501260530e57f3bc7ac1a4b47cdbea
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-26 10:24:21 -0300

    Update 65-solucão-exercicio-guiado while.py

--- Python-Básico(Modulo1)/Aulas-Celular/65-solucão-exercicio-guiado.py
+++ Python-Básico(Modulo1)/Aulas-Celular/65-solucão-exercicio-guiado.py
@@ -17,3 +17,21 @@ while indice < len(nome):
 
 novo_nome += '*'
 print(novo_nome)
+
+# My Exercise
+nome = "João Pedro"
+
+novo_nome = ""
+indice = 0
+
+
+# Repetição de nome estilizado 
+
+while indice < len(nome):
+    pegar_letra = nome[indice] # com o nome e o indice consigo a letra
+
+    novo_nome += f'*{pegar_letra}' # esse += fala que é pra concatenar na string "novo_nome" o resultado do "pegar_letra"
+
+    indice += 1 # faz que so pare de realizar isso ao ter a mesma quantidade de caracteres no indice do nome original 
+    
+print(novo_nome)
