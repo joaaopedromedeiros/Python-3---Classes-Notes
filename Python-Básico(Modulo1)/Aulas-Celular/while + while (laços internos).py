8ed0b9e1a54fe7c3759a3c9404150f88fcb91b74
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-26 15:28:11 -0300

    Update while + while (laços internos).py

--- Python-Básico(Modulo1)/Aulas-Celular/while + while (laços internos).py
+++ Python-Básico(Modulo1)/Aulas-Celular/while + while (laços internos).py
@@ -1,1 +1,9 @@
-.
+# O while ele funciona da da seguinte forma
+# Ele executa um bloco while até que a condição se torne verdadeira. Isso, siginifica que...
+
+# Um while(a) dentro de outro while(b), vai passar por while(b), vai pro (a) e só vai voltar pro while(b) quando ele for verdadeiro.
+
+# ele primeiro executa todo while(a) pra se tornar verdadeiro, e só dps retorna pro while maior.
+
+se eu tiver:
+
