a47f86df93a8cb95d935b9298d0776a0097b4281
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-26 17:21:46 -0300

    Update for-e-break.py

--- Python-Básico(Modulo1)/Aulas-Celular/for /for-e-break.py
+++ Python-Básico(Modulo1)/Aulas-Celular/for /for-e-break.py
@@ -17,3 +17,5 @@ else:
 # continue: volta pro início do bloco de código, pula o que vc falar pra pular  e volta
 # break: para todo o código e não fala o else, pula pra fora de todo o bloco de código
 # else: é executado após todo while ser finalizado/executado
+
+# O while vc não sabe quantas repetições o usuário vai realizar. No for vc sabe... e define
