11d86559bcbd427751bc959d54f035ea7ef9a034
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-26 18:41:34 -0300

    Update Aulas-Celular

---
+++ Python-Básico(Modulo1)/Aulas-Celular/dado tipo list / introduçãolist.py
@@ -0,0 +1,28 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+
+#        +01234
+#        -54321
+string = 'ABCDE'  # 5 caracteres (len)
+# print(bool([]))  # falsy
+# print(lista, type(lista))
+
+
+# ------ÍNDICES IGUAL STRINGS-------
+#        0    1      2              3    4
+#       -5   -4     -3             -2   -1
+
+lista = [123, True, 'Luiz Otávio',  1.2, []]
+lista[-3] = 'Maria'
+print(lista)
+print(lista[2], type(lista[2]))
+
+
+#obs: Não são imutáveis como os outros dados, aqui eu consigo renomear elas tipo ali na lista[-3], o print aparece com o novo valor.
+#obs2: é tipo um array
