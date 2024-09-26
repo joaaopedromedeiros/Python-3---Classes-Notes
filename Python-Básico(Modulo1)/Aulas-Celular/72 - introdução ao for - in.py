589474e5b47ca7a913aac3486b40569b0c686aa6
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-26 15:47:36 -0300

    Update 72 - introdução ao for - in.py

--- Python-Básico(Modulo1)/Aulas-Celular/72 - introdução ao for - in.py
+++ Python-Básico(Modulo1)/Aulas-Celular/72 - introdução ao for - in.py
@@ -9,10 +9,27 @@
 
 # print(repeticoes)
 # print('Aquele laço acima pode ter repetições infinitas')
-texto = 'Python'
 
+# While geralmente é quando não sabe quantas vezes o usuário quer repetir a ação.
+
+
+# Mesmo código/coisa (abaixo) mas com o for e simples!!!
+
+
+texto = 'Python'
 novo_texto = ''
+
 for letra in texto:
     novo_texto += f'*{letra}'
     print(letra)
 print(novo_texto + '*')
+
+
+# A variável "letra" eu que criei, texto é a variável.
+
+for "variável" in texto: 
+     print("variável") 
+
+# vai mostrar todas as letras. O resto do código ali é interando o novo texto estilizando... 
+
+
