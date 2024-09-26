bd8a0373efe7ba3902639c9a60fe3624b3784fca
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-26 19:27:11 -0300

    Update dado tipo list

---
+++ Python-Básico(Modulo1)/Aulas-Celular/dado tipo list /alterando-qualquer-indice.py
@@ -0,0 +1,29 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - apaga um índice
+    clear - limpa a lista
+    extend - estende a lista
+    + - concatena listas
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3
+lista = [10, 20, 30, 40]
+lista.append('Luiz')
+nome = lista.pop()
+lista.append(1233) 
+del lista[-1] #apaga do indice indicado, 
+
+# lista.clear() --> apaga toda a lista 
+
+lista.insert(100, 5) --> (indice,valor adicionado)
+# no lista.clear(), vc adiciona naquele indice e empurra o resto para direita 
+
+print(lista[4])
