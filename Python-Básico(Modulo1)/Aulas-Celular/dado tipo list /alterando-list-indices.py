8b545af94f5a0ad32913575f5aef79f7b66ed144
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-26 19:12:43 -0300

    Update dado tipo list

---
+++ Python-Básico(Modulo1)/Aulas-Celular/dado tipo list /alterando-list-indices.py
@@ -0,0 +1,26 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3   4   5
+lista = [10, 20, 30, 40]
+
+# lista[2] = 300
+# del lista[2] --> não recomendado
+# print(lista) --> atualizou com o 300
+# print(lista[2]) 
+
+lista.append(50) # adiciona na última posição da lista
+lista.pop() # excluir o último da lista, no momento, que no caso é 50
+lista.append(60) 
+lista.append(70)
+ultimo_valor = lista.pop(3)
+print(lista, 'Removido,', ultimo_valor) 
+
+# apenas visualizando até mesmo o valor excluido se colocado em uma variável
