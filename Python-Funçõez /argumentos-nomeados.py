be58f7f022da84e212135084325c1625db7ef523
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-27 19:10:11 -0300

    Update argumentos-nomeados.py

--- Python-Funçõez /argumentos-nomeados.py
+++ Python-Funçõez /argumentos-nomeados.py
@@ -13,4 +13,7 @@ def soma(x, y, z):
 soma(1, 2, 3)
 soma(1, y=2, z=5) # nesse caso não vai depender da ordem respectiva dos parâmetros, pois estou definindo os valores dos argumentos (de cada parâmetro) enquanto chamo a função 
 
+
+# OBS: Todos argumentos precisam ser nomeados caso vc faça a nomeaçâo de pelo menos 1 anterior. Os próximos vão precisar serem também!
+
 print(1, 2, 3, sep='-')
