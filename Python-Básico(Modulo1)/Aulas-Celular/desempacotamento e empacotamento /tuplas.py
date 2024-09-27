e9f3ef4ab00692c7551de7c75e8c1d5f2502522d
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-27 11:16:39 -0300

    Update tuplas.py

--- Python-Básico(Modulo1)/Aulas-Celular/desempacotamento e empacotamento /tuplas.py
+++ Python-Básico(Modulo1)/Aulas-Celular/desempacotamento e empacotamento /tuplas.py
@@ -7,4 +7,4 @@ nomes = ('Maria', 'Helena', 'Luiz')
 print(nomes[-1])
 print(nomes)
 
-# vc só não precisa por o [], de array/lista e cria uma tupla de 3 dados ou mais... ( é mais rápido até que a lista)
+# vc só não precisa por o [], de array/lista e cria uma tupla de 3 dados ou mais... ( é mais rápido até que a lista). Mas como toda string, é imutável 
