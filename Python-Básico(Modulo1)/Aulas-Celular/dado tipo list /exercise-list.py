07533e69715569f8b9529c7fcf072f9757383eb9
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-27 10:55:32 -0300

    Update dado tipo list

---
+++ Python-Básico(Modulo1)/Aulas-Celular/dado tipo list /exercise-list.py
@@ -0,0 +1,27 @@
+
+# lista de cada elemento + indice 
+
+lista = ['João', 'Pedro', 'Araújo']
+
+
+# Pegar cada elemento da lista
+for nome in lista:
+    print(nome)
+    
+    
+#Pegar cada indice da lista com range + len
+
+indices = range(len(lista))
+
+for indice in indices:
+ print(indice, lista[indice])
+ 
+ # Colocar um no lado do outro, como tenho acesso a tudo agora... (acima)
+ 
+
+ 
+
+
+
+
+
