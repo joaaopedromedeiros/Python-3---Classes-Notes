5fdbc6329e6a9dbb50d7f09d994e4db13b792488
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-27 17:28:31 -0300

    Update impressao-numeros-pontos-flutuantes.py

--- Python-Básico(Modulo1)/Aulas-Celular/impressao-numeros-pontos-flutuantes.py
+++ Python-Básico(Modulo1)/Aulas-Celular/impressao-numeros-pontos-flutuantes.py
@@ -7,7 +7,7 @@ https://docs.python.org/pt-br/3/tutorial/floatingpoint.html
 import decimal
 numero_1 = decimal.Decimal('0.1')
 numero_2 = decimal.Decimal('0.7')
-numero_3 = numero_1 + numero_2
+numero_3 = numero_1 + numero_2 # 0,79999999
 print(numero_3)
-print(f'{numero_3:.2f}')
-print(round(numero_3, 2))
+print(f'{numero_3:.2f}') # arredonda mas como string
+print(round(numero_3, 2)) # arredondar pra 0,8 pois é a variável, número de casas decimais (float)
