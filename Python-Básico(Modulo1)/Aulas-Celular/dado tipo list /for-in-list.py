abd8124fcb8ca8919e2cbcb24301cd717755685c
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-27 10:32:38 -0300

    Update dado tipo list

---
+++ Python-Básico(Modulo1)/Aulas-Celular/dado tipo list /for-in-list.py
@@ -0,0 +1,15 @@
+"""
+for in com listas
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+
+for nome in lista:
+    print(nome, type(nome))
+
+# for (nome para nomer os itens da variável, nesse caso a lista, que eu quero) in lista: 
+#    fazer o que com o que eu quero? 
+     print(os dados do que eu quero, cada um em uma string, type(nome-do que eu quero)).
+
+# for nome in lista 
+   print(nome, type(nome)) 
+   
