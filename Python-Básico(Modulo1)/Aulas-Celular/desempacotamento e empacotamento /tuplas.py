b4107a43208fd800d6ab1f39b17b80f68aaf19df
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-27 11:18:19 -0300

    Update tuplas.py

--- Python-Básico(Modulo1)/Aulas-Celular/desempacotamento e empacotamento /tuplas.py
+++ Python-Básico(Modulo1)/Aulas-Celular/desempacotamento e empacotamento /tuplas.py
@@ -7,4 +7,6 @@ nomes = ('Maria', 'Helena', 'Luiz')
 print(nomes[-1])
 print(nomes)
 
-# vc só não precisa por o [], de array/lista e cria uma tupla de 3 dados ou mais... ( é mais rápido até que a lista). Mas como toda string, é imutável 
+# vc só não precisa por o [], de array/lista e cria uma tupla de 3 dados ou mais... ( é mais rápido até que a lista). Mas como toda string, é imutável
+
+# É uma string, mas chama de tuple o dado nesse formato de 3 ou mais strings como está acima. Se vc tentar mudar o dado, vai dar um erro "Tuple dont support item assignment "
