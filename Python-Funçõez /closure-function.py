86ad89c33aae802c68d67bfb5b0f09376ba4633e
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-28 12:12:19 -0300

    Update closure-function

--- Python-Funçõez /closure-function.py
+++ Python-Funçõez /closure-function.py
@@ -13,9 +13,10 @@ Closure e funções que retornam outras funções
 #s1 = criar_saudacao('bom dia', 'Luiz')
 # print( s1() )
 
-# closure ( é isso acima)
+# closure --> é isso acima
 
 
+# Vc basicamente primeiro salva o saudacao e deixa ele fixo, dps é definido os nomes
 
 def criar_saudacao(saudacao):
     def saudar(nome):
