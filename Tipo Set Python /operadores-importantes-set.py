8285d21777ebec9847d8c5ab3e3285fc2cb1e770
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-28 19:09:55 -0300

    Update operadores-importantes-set.py

--- Tipo Set Python /operadores-importantes-set.py
+++ Tipo Set Python /operadores-importantes-set.py
@@ -5,14 +5,22 @@ print(s1)
 # print(s1)
 
 # Operadores úteis:
+
+# Igual matemática 
+
 # união | união (union) - Une
 # intersecção & (intersection) - Itens presentes em ambos
 # diferença - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+
+
 s1 = {1, 2, 3}
 s2 = {2, 3, 4}
-s3 = s1 | s2
-s3 = s1 & s2
-s3 = s2 - s1
-s3 = s1 ^ s2
+
+# Operações 
+
+s3 = s1 | s2 # união 
+s3 = s1 & s2 # Intercessão 
+s3 = s2 - s1 # Diferença 
+s3 = s1 ^ s2 # Itens que não estão em ambos
 print(s3)
