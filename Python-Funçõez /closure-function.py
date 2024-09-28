c3c6092cb2ee4863ac121bb156160e6fcbe3d193
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-28 12:03:44 -0300

    Update closure-function.py

--- Python-Funçõez /closure-function.py
+++ Python-Funçõez /closure-function.py
@@ -3,6 +3,19 @@ Closure e funções que retornam outras funções
 """
 
 
+# def criar_saudacao(saudacao,nome):
+#    def saudar():
+#        return f'{saudacao}, {nome}
+#     return saudar --> devido aqui não ter (), vc coloca no momento do print 
+
+#
+
+#s1 = criar_saudacao(bom dia, Luiz)
+# print( s1() )
+
+
+
+
 def criar_saudacao(saudacao):
     def saudar(nome):
         return f'{saudacao}, {nome}!'
@@ -11,6 +24,10 @@ def criar_saudacao(saudacao):
 
 falar_bom_dia = criar_saudacao('Bom dia')
 falar_boa_noite = criar_saudacao('Boa noite')
+
+
+
+# estrutura de repetição 
 
 for nome in ['Maria', 'Joana', 'Luiz']:
     print(falar_bom_dia(nome))
