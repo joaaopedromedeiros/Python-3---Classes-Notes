9f2016fc6ffd58317559cc001b7698b25bd5e5f8
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-29 12:01:30 -0300

    Update  introdução.py

--- Checar se existe métodos, etc / introdução.py
+++ Checar se existe métodos, etc / introdução.py
@@ -5,5 +5,6 @@ metodo = 'strip' # poderia ser  'upper'
 if hasattr(string, metodo):
     print('Existe strip')
     print(getattr(string, metodo)())
+#getattr --> vai pegar o método
 else:
     print('Não existe o método', metodo)
