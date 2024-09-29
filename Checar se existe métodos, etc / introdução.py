5dfa30e742ac3c470072e6ab44bb4001c126367e
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-29 11:57:32 -0300

    Update  introdução.py

--- Checar se existe métodos, etc / introdução.py
+++ Checar se existe métodos, etc / introdução.py
@@ -1,9 +1,9 @@
 # dir, hasattr e getattr em Python
 string = 'Luiz'
-metodo = 'strip'
+metodo = 'strip' # poderia ser  'upper'
 
 if hasattr(string, metodo):
-    print('Existe upper')
+    print('Existe strip')
     print(getattr(string, metodo)())
 else:
     print('Não existe o método', metodo)
