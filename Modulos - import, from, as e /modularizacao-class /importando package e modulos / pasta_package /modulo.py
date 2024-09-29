da0fefcceed6252c75ef3637c2f6e76667c531d5
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-29 15:21:15 -0300

    Update modulo.py

--- Modulos - import, from, as e /modularizacao-class /importando package e modulos / pasta_package /modulo.py
+++ Modulos - import, from, as e /modularizacao-class /importando package e modulos / pasta_package /modulo.py
@@ -1,2 +1,14 @@
+__all__ = [
+    'variavel',
+    'soma',
+   'nova_variavel',
+]
+
+# esse all define quais variáveis vão ser passadas a importar com má prática pra n passar tudo...
+
+variavel = 'Alguma coisa'
+nova_variavel = 'OK'
+
+
 def soma(x,y):
    return x + y 
