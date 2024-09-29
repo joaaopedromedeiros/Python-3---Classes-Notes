eec6e98c8aee3f4c4a472ddf4804719ba540700b
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-29 15:19:17 -0300

    Update aula9999.py

--- Modulos - import, from, as e /modularizacao-class /importando package e modulos /aula9999.py
+++ Modulos - import, from, as e /modularizacao-class /importando package e modulos /aula9999.py
@@ -1,13 +1,13 @@
-import aula99_package.modulo
-from aula99_package import modulo
-from aula99_package.modulo import * # má prática 
+import pasta_package.modulo
+from pasta_package import modulo
+from pasta_package.modulo import * # má prática 
 
-# from aula99_package.modulo import soma_do_modulo
+# from pasta_package.modulo import soma
 
 # print(*path, sep='\n')
 
 print(soma(1, 2))
-print(aula99_package.modulo.soma(1, 2))
+print(pasta_package.modulo.soma(1, 2))
 print(modulo.soma(1, 2))
 
 # com má prática 
