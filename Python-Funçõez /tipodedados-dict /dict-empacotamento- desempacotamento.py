077407a27d9dc5a643a65a4cf3b90b38e2ad64dd
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-28 21:15:44 -0300

    Update dict-empacotamento- desempacotamento.py

--- Python-Funçõez /tipodedados-dict /dict-empacotamento- desempacotamento.py
+++ Python-Funçõez /tipodedados-dict /dict-empacotamento- desempacotamento.py
@@ -1,28 +1,27 @@
 # Empacotamento e desempacotamento de dicionários
 
-
-
-
-
-# for chave, valor in pessoa.items():
-#     print(chave, valor)
-
 pessoa = {
     'nome': 'Aline',
     'sobrenome': 'Souza',
 }
 
 
+# >>Desempacotando no manual<<
 # (a1, a2), (b1, b2) = pessoa.items() --> desempacotamento (Nome, Aline), (sobrenome, Souza) 
 
 # print(a1, a2)
 # print(b1, b2)
+
+# >>Desempacotando com for in<<
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
 
 
 dados_pessoa = {
     'idade': 16,
     'altura': 1.6,
 }
+
 
 pessoas_completa = {**pessoa, **dados_pessoa}
 # print(pessoas_completa)
