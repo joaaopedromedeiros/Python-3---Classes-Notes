317ac8411ebf362ceac5d9e8e77db39250660b09
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-29 16:40:09 -0300

    Update __init__.py

--- Modulos - import, from, as e /__init__ - Como usar? /aula99_package /__init__.py
+++ Modulos - import, from, as e /__init__ - Como usar? /aula99_package /__init__.py
@@ -1,7 +1,22 @@
 from aula99_package.modulo import *
 from aula99_package.modulo_b import *
 
-# tudo que é importado oara aqui e possível usar como o __algumacoisa__
+
+
+# Observações MUITOOO IMPORTANTE:
+# Em nenhum lugar eu importo esse __init__.py, eu só crio esse arquivo no package (na pasta). Pois esse arquivo vai fazer funcionar esses ngc que mencionei acima apenas importando, por exemplo, pasta_package, sem especificar o arquivo.
+
+# Como? Abaixo;
+# >> import pasta_package << 
+
+# E por ter dentro dessa pasta (pasta_package) o __init__.py, o que tiver importado dentro desse __init__.py eu vou conseguir utilizar nos outros arquivos que eu fizer a importação como citado acima.
+
+
+
+# tudo que é importado para aqui e possível usar como o __algumacoisa__
 # funções,  variáveis,  etc.
 
-# nessa situação eu to importando tudo dos arquivos acima para aqui então aonde eu importar esse __init__.py eu consigo usar!!!! como __AlgumaCoisa__
+# nessa situação eu to importando todos os  arquivos para aqui.  Então aonde eu importar esse a pasta eu consigo usar!!!! como __AlgumaCoisa__
+
+
+
