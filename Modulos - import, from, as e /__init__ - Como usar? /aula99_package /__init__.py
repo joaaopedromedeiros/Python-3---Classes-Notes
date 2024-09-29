1a0cf0e8bbe13ca8c850a787a5eb4f8e3da45d13
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-29 16:30:49 -0300

    Update __init__.py

--- Modulos - import, from, as e /__init__ - Como usar? /aula99_package /__init__.py
+++ Modulos - import, from, as e /__init__ - Como usar? /aula99_package /__init__.py
@@ -1,2 +1,7 @@
 from aula99_package.modulo import *
 from aula99_package.modulo_b import *
+
+# tudo que é importado oara aqui e possível usar como o __algumacoisa__
+# funções,  variáveis,  etc.
+
+# nessa situação eu to importando tudo dos arquivos acima para aqui então aonde eu importar esse __init__.py eu consigo usar!!!! como __AlgumaCoisa__
