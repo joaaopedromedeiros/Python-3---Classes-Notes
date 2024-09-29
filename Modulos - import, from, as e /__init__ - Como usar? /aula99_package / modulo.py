ca30a9b565a8f5aee4c576167c1ff4dba2c8cde2
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-29 16:16:59 -0300

    Update  modulo.py

--- Modulos - import, from, as e /__init__ - Como usar? /aula99_package / modulo.py
+++ Modulos - import, from, as e /__init__ - Como usar? /aula99_package / modulo.py
@@ -3,6 +3,15 @@
 #     'soma_do_modulo',
 #     'nova_variavel',
 # ]
+from aula99_package.modulo_b import fala_oi
 # from aula99_package.modulo_b import fala_oi
 
 variavel = 'Alguma coisa'
+
+
+def soma_do_modulo(x, y):
+    return x + y
+
+
+nova_variavel = 'OK'
+# fala_oi()
