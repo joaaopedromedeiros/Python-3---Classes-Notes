2412f637fa2da22e95d8d5fd4d9d75934ea78ab2
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-29 14:54:59 -0300

    Update  aula98.py

--- Modulos - import, from, as e /modularizacao-class /como recarregar-modulo? / aula98.py
+++ Modulos - import, from, as e /modularizacao-class /como recarregar-modulo? / aula98.py
@@ -1,1 +1,14 @@
-.
+import importlib
+
+import aula98_m
+
+print(aula98_m.variavel)
+
+for i in range(10):
+    importlib.reload(aula98_m) # recarrega
+    print(i)
+print('Fim')
+
+
+print(123)
+variavel = 'Luiz 1'
