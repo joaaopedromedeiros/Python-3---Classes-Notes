a94648e21eec6a9dcc51907e0b924a3e0aa5acf9
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-30 10:51:55 -0300

    Update Ambiente Virtual (venv)

---
+++ Ambiente Virtual (venv) /activate-deactivate-venv.py
@@ -0,0 +1,19 @@
+# Abra a pasta do seu projeto no terminal
+# e digite:
+# python -m venv venv
+#
+# Ativando e desativando meu ambiente virtual
+# Windows: .\venv\Scripts\activate
+
+# Observações: Tudo que vc instalar com o PIP, se o ambiente estiver ativado, vai instalar na pasta, no local, no projeto que vc esta
+
+# Linux e Mac: source venv/bin/activate
+# Desativar: deactivate
+
+# tudo que vc instalar o ambiente virtual do projeto desativado, vc vai instalar fora, globalmente na sua maquina
+
+
+#Curiosidade  MUITO IMPORTANTE
+
+# É sempre bom instalar coisascom ele ativado no ambiente do projeto, pois se vc instalar sempre na sua maquina globalmente, com ele desativado, a cada projeto vc vai instalar tanta coisa que vai fazer ela ficar lenta e travar demais!
+#
