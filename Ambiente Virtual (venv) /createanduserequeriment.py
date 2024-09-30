1a876a0e11c68d0e7fe1fef47678cfe93e3dd4e9
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-30 11:09:17 -0300

    Update createanduserequeriment.py

--- Ambiente Virtual (venv) /createanduserequeriment.py
+++ Ambiente Virtual (venv) /createanduserequeriment.py
@@ -2,7 +2,10 @@
 # Congelar (ver pacotes)
 # pip freeze
 #
-# Criando e usando um requirements.txt
+# Criando o arquivo e usando um requirements.txt
 # pip freeze > requirements.txt
+
 # Instalando tudo do requirements.txt
 # pip install -r requirements.txt
+
+# Vc manda isso pro github pois mandar todos pacotes que vc instalou direto pode ficar enorme. Ai ele lista tudo e vc baixa na hora
