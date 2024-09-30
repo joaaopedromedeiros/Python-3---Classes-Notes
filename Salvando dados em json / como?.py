4c6528da8bc88a0bf21d6ae40e817d9cfd78f22b
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-30 11:34:55 -0300

    Update  como?.py

--- Salvando dados em json / como?.py
+++ Salvando dados em json / como?.py
@@ -1,4 +1,7 @@
 # Posso usar qualquer tipo de dados, ou arquivo py! Nesse caso ele usou o dicionário apenas por ser mais complexo e mostrar que dá certo.
 
 #1) add import json, no arquivo que deseja salvar os dados em json
-#2)
+#2) Vc utiliza o código with...
+
+
+# Obs: Funções,  métodos,  etc é mais complicado e não é bom salvar em json!  
