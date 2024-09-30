f6e6e99cb26c95822d6aae9ca282ed064d482196
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-30 20:47:42 -0300

    Update 1011.py

--- Exercícios IF / Python Básico /lista 3 /1011.py
+++ Exercícios IF / Python Básico /lista 3 /1011.py
@@ -2,3 +2,11 @@
 # v = 4/3 * pi * r³
 
 # pi = 3.14159
+
+raio = int(input("Qual o raio da sua esfera? "))
+
+pi = 3.14159
+volume = (4/3) * pi * (raio ** 3)
+
+print(f"O volume em m³ da sua esfera é {volume:.5f}")
+
