44503f2dc56282114b0a2338871cc3d490c5eeba
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-30 20:42:29 -0300

    Update 1005.py

--- Exercícios IF / Python Básico /lista 3 /1005.py
+++ Exercícios IF / Python Básico /lista 3 /1005.py
@@ -2,14 +2,13 @@
 
 
 
-entrada = int(input("Digite um numero: "))
-entrada_2 = int(input("Digite outro numero: "))
+entrada = float(input("Digite um numero: "))
+entrada_2 = float(input("Digite outro numero: "))
 
 
 # pesos
 # 3.5 - entrada
 # 7.5 - entrada 2
-media_ponderada = ( (entrada * 3.5) + (entrada_2 * 3.5) ) / 11
+media_ponderada = ( (entrada * 3.5) + (entrada_2 * 7.5) ) / 11
 
 print(f'Média: {media_ponderada:.5f}')
-
