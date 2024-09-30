9a756494af5e8e47d9434223c99ca0ca02869e6a
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-30 20:38:58 -0300

    Update 1005.py

--- Exercícios IF / Python Básico /lista 3 /1005.py
+++ Exercícios IF / Python Básico /lista 3 /1005.py
@@ -1,2 +1,15 @@
 #média ponderada
 
+
+
+entrada = int(input("Digite um numero: "))
+entrada_2 = int(input("Digite outro numero: "))
+
+
+# pesos
+# 3.5 - entrada
+# 7.5 - entrada 2
+media_ponderada = ( (entrada * 3.5) + (entrada_2 * 3.5) ) / 11
+
+print(f'Média: {media_ponderada:.5f}')
+
