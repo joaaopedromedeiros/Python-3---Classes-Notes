277bcbfb9a08f2b3c2d35f73d0ea7fc9c68db15f
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-30 11:14:49 -0300

    Update createanduserequeriment.py

--- Ambiente Virtual (venv) /createanduserequeriment.py
+++ Ambiente Virtual (venv) /createanduserequeriment.py
@@ -8,6 +8,13 @@
 # Instalando tudo do requirements.txt
 # pip install -r requirements.txt
 
-# Vc manda isso pro github pois mandar todos pacotes que vc instalou direto pode ficar enorme. Ai ele lista tudo e vc baixa na hora
+# Como atualizar com novos pacotes que coloquei no projeto? Para atualizar isso com os novos e manter os antigos é só fazer a mesma coisa novamente 
+
+pip freeze > requirements.txt
+
+
+# OBSERVAÇÃO 
+
+# Vc manda isso pro github pois mandar todos pacotes que vc instalou direto pode ficar enorme. Ai ele lista tudo e vc baixa na hora 
 
 # Em todo projeto que não tiver o venv e tiver o requirements.txt, vc abre a pasta do projeto, cria o ambiente virtual novo e chama o requirments com o pip que vai automaticamente instalar tudo que precisa
