6b7ae9e5577ee3dfabeb9f5815d4c5aff5b0f563
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-01 10:32:13 -0300

    Update introduçãoao__init__.py

--- POO - Introdução /introduçãoao__init__.py
+++ POO - Introdução /introduçãoao__init__.py
@@ -26,13 +26,18 @@ class Pessoa:
 # 1°) Usa a função def __init__(Parâmetros): ..
 # 2°) Precisa ser o primeiro param o self, obrigatoriamente, e dps vc pd passar o resto.
 
+>>>>>
 # O que acontece com esse self? Assim que vc a classe dentro de uma instância (objeto), ex:  "p1",  o python já chama ele automaticamente. 
+
+# Como o self assume o valor de "p1" é como se tivesse fazendo p1.nome = algo, mas fica self.nome = algo 
+
+>>>>>>>>>
 
 # 3°) Cria os atributos com self.parametro = 
 
 p1 = Pessoa('Luiz', 'Otávio') # argumentos nome,sobrenome  
 
-# p1.nome = 'Luiz'
+# p1.nome = 'Luiz' --> p1 vira self 
 # p1.sobrenome = 'Otávio'
 
 p2 = Pessoa('Maria', 'Joana')
