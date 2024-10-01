03a8bb73e23b70a06f821cf2e723ee6262cb1de6
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-09-30 21:00:17 -0300

    Update 1015.py

--- Exercícios IF / Python Básico /lista 3 /1015.py
+++ Exercícios IF / Python Básico /lista 3 /1015.py
@@ -1,1 +1,10 @@
 #distância entre dois pontos
+
+# digite as distâncias de um ponto cartesiano 
+# x1 & y1
+
+# digite as distâncias de outro pontos cartesianos
+# x2 & y2
+
+
+# ache a distância entre eles ( Repare que isso vai gerar um triangulo retangulo, logo apenas utilizo a formula de pitagoras ) 
