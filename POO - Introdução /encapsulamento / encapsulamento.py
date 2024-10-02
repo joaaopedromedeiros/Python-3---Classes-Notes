e3b069b5e5b46db6a94ae5ce06ef7720f24f1136
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-02 18:37:26 -0300

    Update  encapsulamento.py

--- POO - Introdução /encapsulamento / encapsulamento.py
+++ POO - Introdução /encapsulamento / encapsulamento.py
@@ -19,19 +19,23 @@ class Foo:
         self.public = 'isso é público'
         self._protected = 'isso é protegido'
 
-# não acesse/use isso no código com print, variáveis,  etc
+# não acesse/use isso no código com print, variáveis,  etc. Use apenas dentro da classe 
 
         self.__exemplo = 'isso é private'
 
     def metodo_publico(self):
         # self._metodo_protected()
         # print(self._protected)
+
+# >>> Você pd usar esse método _protected em qualquer da SUA CLASSE mas fora dela não, por isso posso chamar ela no método público mas dentro da classe. 
+
         print(self.__exemplo)
         self.__metodo_private()
         return 'metodo_publico'
 
     def _metodo_protected(self):
-# Apenas utiliza na classe,  não chama ela.
+# Apenas utiliza dentro da classe,  não chama ela fora! 
+
         print('_metodo_protected')
         return '_metodo_protected'
 
@@ -40,7 +44,7 @@ class Foo:
         return '__metodo_private'
 
 
-# Fora da classe (tudo que ta abaixo) 
+# Fora da classe (tudo que ta abaixo) abaixo
 
 f = Foo()
 # print(f.public)
