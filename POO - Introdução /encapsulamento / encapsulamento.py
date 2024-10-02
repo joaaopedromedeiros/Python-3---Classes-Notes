ea56332bfcd20f813a3462354747240baae3310f
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-02 18:39:44 -0300

    Update  encapsulamento.py

--- POO - Introdução /encapsulamento / encapsulamento.py
+++ POO - Introdução /encapsulamento / encapsulamento.py
@@ -1,13 +1,21 @@
 # Encapsulamento (modificadores de acesso: public, protected, private)
 # Python NÃO TEM modificadores de acesso
+
 # Mas podemos seguir as seguintes convenções
 #   (sem underline) = public
+
 #       pode ser usado em qualquer lugar
 # _ (um underline) = protected
+
 #       não DEVE ser usado fora da classe
 #       ou suas subclasses.
 # __ (dois underlines) = private
+
+# >>> Não pode ser usado nas classes filhas!
+
+
 #       "name mangling" (desfiguração de nomes) em Python
+
 #       _NomeClasse__nome_attr_ou_method
 #       só DEVE ser usado na classe em que foi
 #       declarado.
