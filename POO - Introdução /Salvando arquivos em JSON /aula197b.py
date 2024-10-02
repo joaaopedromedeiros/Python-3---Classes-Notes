40bc2b05bfaa27c15d5211ea81b20d6682cf5bd7
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-02 10:06:46 -0300

    Update aula197b.py

--- POO - Introdução /Salvando arquivos em JSON /aula197b.py
+++ POO - Introdução /Salvando arquivos em JSON /aula197b.py
@@ -10,15 +10,18 @@ from aula127_a import CAMINHO_ARQUIVO, Pessoa, fazer_dump # precisa
 
 with open(CAMINHO_ARQUIVO, 'r') as arquivo:
     pessoas = json.load(arquivo)
-    p1 = Pessoa(**pessoas[0])
+
+
+    p1 = Pessoa(**pessoas[0]) # expandido dicionário, igualmente no p2 e p3n
     p2 = Pessoa(**pessoas[1])
     p3 = Pessoa(**pessoas[2])
+
     print(p1.nome, p1.idade)
     print(p2.nome, p2.idade)
     print(p3.nome, p3.idade)
 
 # with open(Caminho que vc definiu, 'r'), as variável definida:
- algo = json.load(variável que vc definiu) 
+# algo = json.load(variável que vc definiu) 
 
 
 # r --> read 
