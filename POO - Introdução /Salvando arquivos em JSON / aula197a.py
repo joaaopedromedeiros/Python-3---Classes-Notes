b8570845d4a1bd779b9dbcb40a8e78158f3f7723
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-02 09:48:07 -0300

    Update  aula197a.py

--- POO - Introdução /Salvando arquivos em JSON / aula197a.py
+++ POO - Introdução /Salvando arquivos em JSON / aula197a.py
@@ -1,1 +1,8 @@
 # Salvar arquivos
+
+
+# Exercício - Salve sua classe em JSON
+# Salve os dados da sua classe em JSON
+# e depois crie novamente as instâncias
+# da classe com os dados salvos
+# Faça em arquivos separados.
