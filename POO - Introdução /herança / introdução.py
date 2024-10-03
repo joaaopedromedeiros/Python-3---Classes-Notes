0ae97830a946583abbc71abc8d65ed2a6329b212
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-03 19:26:38 -0300

    Update  introdução.py

--- POO - Introdução /herança / introdução.py
+++ POO - Introdução /herança / introdução.py
@@ -1,6 +1,9 @@
 # Herança simples - Relações entre classes
 # Associação - usa, Agregação - tem
 # Composição - É dono de, Herança - É um
+
+# Na associação apenas usa, na agregação usa e tem, e na composição usa, tem e é dono
+
 #
 # Herança vs Composição
 #
