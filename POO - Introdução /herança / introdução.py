8448b1acc07a0bc2a7440fa1ef7cc3b23839bd36
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-03 19:28:48 -0300

    Update  introdução.py

--- POO - Introdução /herança / introdução.py
+++ POO - Introdução /herança / introdução.py
@@ -2,7 +2,9 @@
 # Associação - usa, Agregação - tem
 # Composição - É dono de, Herança - É um
 
-# Na associação apenas usa, na agregação usa e tem, e na composição usa, tem e é dono
+# Na associação apenas usa, na agregação usa e tem, e na composição usa, tem e é dono. A Herança 
+
+# Posso ter um tipo que extende o seu tipo por Herança... possuindo um tipo. 
 
 #
 # Herança vs Composição
