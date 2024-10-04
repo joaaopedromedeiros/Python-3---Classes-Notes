05046f934f07c92076a1ccb16cfeb67eaaf8c6c3
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-04 16:35:35 -0300

    Update log.py

--- POO - Introdução /Mixins, abstração e união  /log.py
+++ POO - Introdução /Mixins, abstração e união  /log.py
@@ -4,7 +4,7 @@ class Log:
         raise NotImplementedError('Implemente o método log')
    # eu não quero que as pessoas use esse metodo diretamente, 
 #  ai vc pede pra usar outro com o que vc informa na mensagem do erro
-
+# não quer que vc use a super classe, a classe mãe.  Ai vc implementa erro e diz a classe que deve usar que é filha dessa super classe ( herda os dados, etc ) 
 # raise --> implementar um erro pra teste
 
 class LogFileMixin(Log):
