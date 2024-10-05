97813f343a212cfeda1d26bd4f726913fd6b2137
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-05 14:08:04 -0300

    Update dunder__repr__and__str__.py

--- POO - Introdução /Special Method (__Methods__) /dunder__repr__and__str__.py
+++ POO - Introdução /Special Method (__Methods__) /dunder__repr__and__str__.py
@@ -1,4 +1,8 @@
 # Python Dunder Methods __repr__ e __str__
+# Ambos métodos servem para mostrar o valor
+# em string, mas há diferenças.
+
+
 # Dunder = Double Underscore = __dunder__
 # Antigo e útil: https://rszalski.github.io/magicmethods/
 # https://docs.python.org/3/reference/datamodel.html#specialnames
@@ -21,15 +25,22 @@ class Ponto:
         self.y = y
         self.z = z
 
+# __str__ = Geralmente usado para mostrar ao
+# usuário, os valores dos argumentos dos objetos que usam a classe
+
     def __str__(self):
         return f'({self.x}, {self.y})'
+
+# __repr__ = Usado para mostrar aos
+# desenvolvedores como você deseja mostrar os 
+# valores
 
     def __repr__(self):
         # class_name = self.__class__.__name__
         class_name = type(self).__name__
         return f'{class_name}(x={self.x!r}, y={self.y!r}, z={self.z!r})'
 
-
+# p1 = um objeto, Classe(Argumento1,Argumento2) 
 p1 = Ponto(1, 2)
 p2 = Ponto(978, 876)
 print(p1)
