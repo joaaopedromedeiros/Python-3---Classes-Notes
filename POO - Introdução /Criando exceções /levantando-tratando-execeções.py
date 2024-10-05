6dd4745e820269f8d915100e049506d8e462619c
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-04 23:50:05 -0300

    Update Criando exceções

---
+++ POO - Introdução /Criando exceções /levantando-tratando-execeções.py
@@ -0,0 +1,29 @@
+# Levantando e tratando suas Exceptions (Exceções)
+# https://docs.python.org/3/library/exceptions.html
+
+# Levantando (raise) / Lançando (throw) exceções
+# Relançando exceções
+# Adicionando notas em exceções (3.11.0)
+
+# CÓDIGO ABAIXO
+
+
+class MeuError(Exception):
+    ...
+class OutroError(Exception):
+    ...
+def levantar():
+    exception_ = MeuError('a', 'b', 'c')
+    raise exception_
+try:
+    levantar()
+except (MeuError, ZeroDivisionError) as error:
+    print(error.__class__.__name__)
+    print(error.args)
+    print()
+    exception_ = OutroError('Vou lançar de novo')
+    raise exception_ from error
+
+# OBSERVAÇÕES
+
+# >> ...
