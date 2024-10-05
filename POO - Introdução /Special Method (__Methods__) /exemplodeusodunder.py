e29dfc97fe63bb5f94dcba316708d53f03898a23
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-05 14:31:41 -0300

    Update exemplodeusodunder.py

--- POO - Introdução /Special Method (__Methods__) /exemplodeusodunder.py
+++ POO - Introdução /Special Method (__Methods__) /exemplodeusodunder.py
@@ -53,5 +53,18 @@ if __name__ == '__main__':
 # >> other.x? other.y?
 # Eu ativo o _add_, ou outro método, 
 # para usar com  objetos que possuem a mesma
-# classe! Então esse other.x é o da outra classe
-# que estiver somando. 
+# classe! Então esse other.x é o do outro objeto
+# da mesma classe que estiver somando. 
+
+
+# >> sobre a operação p1 + p2
+# Eu poderia retornar qualquer coisa ao realizar
+# essa operação entre objetos! 
+# Abaixo: 
+# def __add__(self, other):
+#        novo_x = self.x + other.x
+#        novo_y = self.y + other.y
+#        return Ponto(novo_x, novo_y)
+# return = poderia ser qualquer coisa! Mas...
+# Eu criei uma lógica no escopo da def utilizando os 
+# atributos self.x e self.y e retornei oq eu criei
