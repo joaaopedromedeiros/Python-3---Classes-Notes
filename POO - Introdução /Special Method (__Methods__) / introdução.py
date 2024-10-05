7eca65ff4e58a32180a9b6b2bcf33b0a2794dccb
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-05 11:55:36 -0300

    Update  introdução.py

--- POO - Introdução /Special Method (__Methods__) / introdução.py
+++ POO - Introdução /Special Method (__Methods__) / introdução.py
@@ -3,9 +3,17 @@
 # Antigo e útil: https://rszalski.github.io/magicmethods/
 # https://docs.python.org/3/reference/datamodel.html#specialnames
 # __lt__(self,other) - self < other
+" acima, checa se um objeto é menor ou igual "
+
 # __le__(self,other) - self <= other
+" menor ou igual que outro objeto "
+
 # __gt__(self,other) - self > other
+" greater than... maior que outro objeto "
+
 # __ge__(self,other) - self >= other
+" greatest or equal, maior ou igual a outro objeto 
+
 # __eq__(self,other) - self == other
 # __ne__(self,other) - self != other
 # __add__(self,other) - self + other
@@ -15,3 +23,5 @@
 # __neg__(self) - -self
 # __str__(self) - str
 # __repr__(self) - str
+
+# É basicamente comparações entre objetos de acordo com os sinais comparativos
