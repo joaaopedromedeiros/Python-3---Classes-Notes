0d9ff482768e45065e913ead2255e0e7ae51ce81
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-05 15:30:24 -0300

    Update  introdução.py

--- POO - Introdução /Context manager / introdução.py
+++ POO - Introdução /Context manager / introdução.py
@@ -24,18 +24,38 @@ class MyOpen:
         self.modo = modo
         self._arquivo = None
 
+# Primeira coisa que executa
+
     def __enter__(self):
         print('ABRINDO ARQUIVO')
         self._arquivo = open(self.caminho_arquivo, self.modo, encoding='utf8')
         return self._arquivo
+
+
+# Secunda coisa que executa 
+
 
     def __exit__(self, class_exception, exception_, traceback_):
         print('FECHANDO ARQUIVO')
         self._arquivo.close()
 
 
+# Terceira coisa que executa 
+
+
+#CÓDIGO 
+
+# instância = MyOpen('aula149.txt', 'w')
+
 with MyOpen('aula149.txt', 'w') as arquivo:
+
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.write('Linha 3\n')
+
+    print('WITH', arquivo) as arquivo:
     arquivo.write('Linha 1\n')
     arquivo.write('Linha 2\n')
     arquivo.write('Linha 3\n')
     print('WITH', arquivo)
+
