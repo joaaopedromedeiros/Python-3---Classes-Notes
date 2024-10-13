71c6a44af3dded5db0d9982d23215fd50fa8468b
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-12 21:38:27 -0300

    Update  modulo.py

--- POO - Modulo 2 /aula273 / modulo.py
+++ POO - Modulo 2 /aula273 / modulo.py
@@ -1,2 +1,11 @@
 def soma(x: float, y: float):
  return x + y
+
+
+print(__name__) 
+# Aqui vc percebe que caso execute
+# vai aparecer __main__.
+
+# Se importar o modulo.py para outro arquivo 
+# e executar ele lá, o print aparece como "modulo" pois é o nome do arquivo
+print(soma(12,3))
