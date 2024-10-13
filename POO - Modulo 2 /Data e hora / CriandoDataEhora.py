0935b9c57c9c0a679b4612b12cd8fd51a41cf0f6
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-12 22:01:00 -0300

    Update  CriandoDataEhora.py

--- POO - Modulo 2 /Data e hora / CriandoDataEhora.py
+++ POO - Modulo 2 /Data e hora / CriandoDataEhora.py
@@ -19,7 +19,10 @@ from datetime import datetime
 data_str_data = '2022/04/20 07:49:23'
 data_str_data = '20/04/2022'
 data_str_fmt = '%d/%m/%Y'
+               #dia/mês/ANO
 
 # data = datetime(2022, 4, 20, 7, 49, 23)
 data = datetime.strptime(data_str_data, data_str_fmt)
+# data, formatação
+
 print(data)
