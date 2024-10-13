ffda072dfde6ba1f36bf868f2988bc2453c4ddea
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-12 22:03:00 -0300

    Update DataEhoraAtual.py

--- POO - Modulo 2 /Data e hora /DataEhoraAtual.py
+++ POO - Modulo 2 /Data e hora /DataEhoraAtual.py
@@ -1,1 +1,8 @@
-.
+data = datetime.now()
+print(data.timestamp())  # Isso está na base de dados
+print(datetime.fromtimestamp(1670849077))
+# data_str_data = '2022/04/20 07:49:23'
+# data_str_data = '20/04/2022'
+# data_str_fmt = '%d/%m/%Y'
+# data = datetime(2022, 4, 20, 7, 49, 23, tzinfo=timezone('Asia/Tokyo'))
+# data = datetime.strptime(data_str_data, data_str_fmt)
