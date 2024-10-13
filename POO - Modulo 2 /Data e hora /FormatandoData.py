2118e1d463f36a5626f622e151c9025f5d689e25
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-13 11:28:53 -0300

    Update FormatandoData.py

--- POO - Modulo 2 /Data e hora /FormatandoData.py
+++ POO - Modulo 2 /Data e hora /FormatandoData.py
@@ -3,7 +3,16 @@
 # https://docs.python.org/3/library/datetime.html
 from datetime import datetime
 # data = datetime(2022, 12, 13, 7, 59, 23)
-data = datetime.strptime('2022-12-13 07:59:23', '%Y-%m-%d %H:%M:%S')
+
+
+data = datetime.strptime('2022-12-13 07:59:23', '%Y-%m-%d %H:%M:%S') 
+
+# Aqui acima criando a data eu não posso errar
+# a ordem do formato. Abaixo, eu consigo 
+# modificar como eu quero
+
+
+
 print(data.strftime('%d/%m/%Y'))
 print(data.strftime('%d/%m/%Y %H:%M'))
 print(data.strftime('%d/%m/%Y %H:%M:%S'))
