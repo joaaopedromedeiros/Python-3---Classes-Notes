66ef6fe5c1f1bab17720d4fde82fdcf28302da51
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-25 22:50:43 -0300

    Update  ex1.py

--- Exercícios IF /FLA / Lista 1 / ex1.py
+++ Exercícios IF /FLA / Lista 1 / ex1.py
@@ -1,1 +1,14 @@
-.
+i = 0
+
+while i == 0:
+    try:
+        numero = int(input("Digite um número para dobra-lo: "))
+        if numero == 0:
+            raise ValueError()
+        dobrar = numero * 2
+        print(dobrar)
+        i = 1
+    except ValueError as e:
+        print(e)
+        
+        
