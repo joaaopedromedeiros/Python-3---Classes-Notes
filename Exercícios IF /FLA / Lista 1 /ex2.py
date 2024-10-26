37771dabb22a77532b3df1629901859f6be6ddc7
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-25 23:07:27 -0300

    Update ex2.py

--- Exercícios IF /FLA / Lista 1 /ex2.py
+++ Exercícios IF /FLA / Lista 1 /ex2.py
@@ -1,1 +1,16 @@
-.
+i = 0
+
+while i == 0:
+    try:
+        numero_1 = int(input("Digite 1 número: "))
+        numero_2 = int(input("Digite outro número: "))
+        numero_3 = int(input("Digite outro número: "))
+        
+        soma = numero_1 + numero_2 + numero_3
+        print(soma)
+        
+        
+    except ValueError as e:
+        print("Vc digitou algum valor invalido")
+        
+        
