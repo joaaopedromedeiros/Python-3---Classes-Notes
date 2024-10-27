fff2fcc590b2124a52574c273e5b372184889268
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-26 22:10:53 -0300

    Update  Lista 1

---
+++ Exercícios IF /FLA / Lista 1 /ex11.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um número para verificar se seu intervalo estar fora de  [0,10]: "))
+
+intervalo = 0 <= numero >= 10
+# zero é menor ou igual a numero
+# numero é maior ou igual a 10
+print(intervalo)
