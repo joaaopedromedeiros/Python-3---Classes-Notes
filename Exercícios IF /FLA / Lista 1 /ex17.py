f12bff4e857a993aa1198113ffcf893f60289622
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-26 23:22:05 -0300

    Update ex17.py

--- Exercícios IF /FLA / Lista 1 /ex17.py
+++ Exercícios IF /FLA / Lista 1 /ex17.py
@@ -1,1 +1,13 @@
-.
+print("Turnos: M - Matutino, V - Vespertino, N - Nortuno ")
+turno = input("Digite a letra correspondente ao seu turno: ")
+
+
+
+if turno == "M":
+    print("Você estuda no turno matutino")
+elif turno == "V":
+        print("Você estuda no turno vespertino")
+elif turno == "N":
+        print("Você estuda no turno Noturno")
+else:
+    print("Valor invalido")
