7cc358403ecaac01fec9fe99deec1319cb2bd201
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-28 11:04:57 -0300

    Update ex11.py

--- Exercícios IF /FLA /lista 4 /ex11.py
+++ Exercícios IF /FLA /lista 4 /ex11.py
@@ -1,1 +1,14 @@
-.
+a = int(input("Defina até qual número vc deseja ver os pares compreendidos no intervalo: "))
+
+lista_pares = []
+
+def pares(a):
+    for x in range(0, a + 1,2):
+        print(x)
+        lista_pares.append(x)
+        print(lista_pares)
+    print("Resultado final: ")
+    print(lista_pares)
+
+pares(a)
+
