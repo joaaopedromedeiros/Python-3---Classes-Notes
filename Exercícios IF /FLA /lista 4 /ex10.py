afff288faddf7a21d0f79d515b2d80cd33cb5301
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-28 11:08:26 -0300

    Update ex10.py

--- Exercícios IF /FLA /lista 4 /ex10.py
+++ Exercícios IF /FLA /lista 4 /ex10.py
@@ -1,1 +1,15 @@
-.
+a = int(input("Faça a contagem de números até: "))
+
+
+lista = []
+
+def contagem(a):
+    for x in range(1, a + 1):
+        print(x)
+        lista.append(x)
+        print(lista)
+    print("Resultado final: ")
+    print(lista)
+        
+
+contagem(a)
