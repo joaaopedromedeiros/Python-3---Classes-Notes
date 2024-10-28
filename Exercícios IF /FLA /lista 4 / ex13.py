717c9478be8eb33821f052f6d76a5a1b4e1208b3
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-10-28 10:45:38 -0300

    Update  ex13.py

--- Exercícios IF /FLA /lista 4 / ex13.py
+++ Exercícios IF /FLA /lista 4 / ex13.py
@@ -1,1 +1,35 @@
-.
+
+a = int(input("Digite um número para encontrar multiplos: "))
+n = int(input("Quantos multiplos vc quer? "))
+
+
+lista_a = []
+quantidade = []
+
+def multiplos(a,n):
+    
+    for x in range(1, n + 1):
+        quantidade.append(x)
+    print(quantidade)
+    
+    for valor in quantidade:
+        multiplo = a * valor
+        lista_a.append(multiplo)
+        
+    print(lista_a)
+        
+
+multiplos(a,n)
+        
+        
+
+# escrever uma lista com a quantidade de
+# N números que ele quer...
+# a partir dos valores da lista,
+# multiplica ele 
+        
+
+
+    
+    
+    
