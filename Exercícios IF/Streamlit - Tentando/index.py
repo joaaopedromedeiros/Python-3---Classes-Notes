deb21f54b1d6f4a484564a3ffa7a71463f8ae80d
João A. Medeiros <joaopedroaraujodemedeiros@gmail.com>
2024-11-26 15:06:57 -0300

    StreamlitTreino

---
+++ Exercícios IF/Streamlit - Tentando/index.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+
+st.header("Oi")
+df = pd.DataFrame(
+    [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 5, "is_widget": False},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
+)
+
+st.dataframe(df, use_container_width=True)
+
+
+
+animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
+
+animal = st.text_input('Type an animal')
+
+if st.button('Check availability'):
+    have_it = animal.lower() in animal_shelter
+    'We have that animal!' if have_it else 'We don\'t have that animal.'
